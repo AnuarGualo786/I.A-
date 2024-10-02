ab72b2ca71da81c3cae16e70a38d19143be3aad1
Berth <64146128+AnuarGualo786@users.noreply.github.com>
2024-10-02 16:01:36 -0600

    Prac1.py

---
+++ Prac1.py
@@ -0,0 +1,22 @@
+# Imprimir "Hola, como estan" en Python
+print("Hola, como estan. soy un Saiyajin llamado Alberto Gualo")
+
+# Operaciones matemáticas básicas
+a = 10
+b = 20
+c = 30
+# Suma
+suma = a + b + c
+print(f"La suma de {a}, {b} y {c} es: {suma}")
+
+# Resta
+resta = c - b - a
+print(f"La resta de {c}, {b} y {a} es: {resta}")
+
+# Multiplicación
+multiplicacion = a * b * c
+print(f"La multiplicación de {a},  {b} y {c} es: {multiplicacion}")
+
+# División
+division = a / (b * c)
+print(f"La división de {a} entre {b} multiplicado por {c} es: {division}")
